42d03644069ab28131c07b582222f5dc90cd89ba
Kairat <k.batyrbayev@gmail.com>
2018-11-06 18:04:48 +0600

    left side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
+import * as Popper from 'popper.js/dist/umd/popper.js';
 import * as $ from 'jquery';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,8 +11,9 @@ export class AppComponent {
   @ViewChild('pdfContainer') private pdfContainer: ElementRef;
 
   title = 'ng-pdf-highlighter';
+  comment: string = '';
 
-  rect: { x1: number, y1: number, x2: number, y2: number } = { x1: 0, y1: 0, x2: 0, y2: 0 };
+  rect: Rectangle = { x1: 0, y1: 0, x2: 0, y2: 0, width: 0, height: 0 };
   lastMousePosition: Position = { x: 0, y: 0 };
   canvasPosition: Position = { x: 0, y: 0 };
   mousePosition: Position = { x: 0, y: 0 };
@@ -26,72 +27,88 @@ export class AppComponent {
   element = null;
   dataPageNumber: number;
   mouseEvent(event) {
-    if (event.type === 'mousemove') {
 
-      this.mousePosition = {
-        x: event.clientX - this.pagePosition.x,
-        y: event.clientY - this.pagePosition.y
-      };
-
-      if (this.mouseDownFlag) {
-        let width = this.mousePosition.x - this.lastMousePosition.x;
-        let height = this.mousePosition.y - this.lastMousePosition.y;
-        this.rect = {
-          x1: this.lastMousePosition.x,
-          y1: this.lastMousePosition.y,
-          x2: width,
-          y2: height
-        };
-
-        if (this.element != null) {
-          this.element.style.width = width + 'px';
-          this.element.style.height = height + 'px';
-          document.getElementsByClassName('to-draw-rectangle')[this.dataPageNumber - 1].appendChild(this.element);
-        }
-
-      }
-      
-
-    }
-
-    if (event.type === 'mousedown') {
-      this.mouseDownFlag = true;
-
-      let eventPath = event.path.find(p => p.className == 'page');
-      if (typeof eventPath !== 'undefined') {
-        this.dataPageNumber = parseInt(eventPath.getAttribute('data-page-number')); // get id of page
-
-        let toDrawRectangle = document.getElementsByClassName('to-draw-rectangle');
-        let pageOffset = toDrawRectangle[this.dataPageNumber-1].getBoundingClientRect();
-        this.pagePosition = { 
-          x: pageOffset.left,
-          y: pageOffset.top
-        };
-        
-
-        this.lastMousePosition = {
+    if (!this.showPopup) {
+      if (event.type === 'mousemove') {
+  
+        this.mousePosition = {
           x: event.clientX - this.pagePosition.x,
           y: event.clientY - this.pagePosition.y
+        };
+  
+        if (this.mouseDownFlag) {
+          let width = this.mousePosition.x - this.lastMousePosition.x;
+          let height = this.mousePosition.y - this.lastMousePosition.y;
+          this.rect = {
+            x1: this.lastMousePosition.x,
+            y1: this.lastMousePosition.y,
+            x2: this.mousePosition.x,
+            y2: this.mousePosition.y,
+            width: width,
+            height: height
+          };
+  
+          if (this.element != null) {
+            this.element.style.width = width + 'px';
+            this.element.style.height = height + 'px';
+            if (this.rect.width > 0 && this.rect.height > 0) {
+              document.getElementsByClassName('to-draw-rectangle')[this.dataPageNumber - 1].appendChild(this.element);
+            }
+          }
+  
         }
-
-        this.element = document.createElement('div');
-        this.element.className = 'rectangle';
-        this.element.style.position = 'absolute';
-        this.element.style.border = '1px solid black';
-        this.element.style.borderRadius = '3px';
-        this.element.style.background = 'yellow';
-        this.element.style.opacity = 0.4;
-        this.element.style.left = this.lastMousePosition.x + 'px';
-        this.element.style.top = this.lastMousePosition.y + 'px';
+        
+  
       }
-
-      
-      
+  
+      if (event.type === 'mousedown') {
+        this.mouseDownFlag = true;
+  
+        let eventPath = event.path.find(p => p.className == 'page');
+        if (typeof eventPath !== 'undefined') {
+          this.dataPageNumber = parseInt(eventPath.getAttribute('data-page-number')); // get id of page
+  
+          let toDrawRectangle = document.getElementsByClassName('to-draw-rectangle');
+          let pageOffset = toDrawRectangle[this.dataPageNumber-1].getBoundingClientRect();
+          this.pagePosition = { 
+            x: pageOffset.left,
+            y: pageOffset.top
+          };
+          
+  
+          this.lastMousePosition = {
+            x: event.clientX - this.pagePosition.x,
+            y: event.clientY - this.pagePosition.y
+          }
+  
+          let rectId = document.getElementsByClassName('rectangle').length + 1;
+  
+          this.element = document.createElement('div');
+          this.element.className = 'rectangle';
+          this.element.id = 'rectangle-'+rectId;
+          this.element.style.position = 'absolute';
+          this.element.style.border = '2px solid #0084FF';
+          this.element.style.borderRadius = '3px';
+          this.element.style.left = this.lastMousePosition.x + 'px';
+          this.element.style.top = this.lastMousePosition.y + 'px';
+        }
+        
+      }
+  
+      if (event.type === 'mouseup') {
+  
+        this.mouseDownFlag = false;
+        if (this.rect.height > 0 && this.rect.width > 0) {
+          let popper = document.querySelector('.js-popper');
+          new Popper(this.element, popper, {
+            placement: 'top-end'
+          });
+          this.showPopup = true;
+        }
+  
+      }
     }
 
-    if (event.type === 'mouseup') {
-      this.mouseDownFlag = false;
-    }
 
   }
 
@@ -105,14 +122,61 @@ export class AppComponent {
     elem.style.right = 0 + 'px';
     elem.style.bottom = 0 + 'px';
     elem.style.cursor = 'crosshair';
-    elem.style.background = 'red';
-    elem.style.opacity = '0.4';
+    // elem.style.background = 'red';
+    // elem.style.opacity = '0.4';
     event.path.find(p => p.className == 'page').appendChild(elem);
+    $('.textLayer').addClass('disable-textLayer');
   }
+
+
+  showPopup: boolean = false;
+  getStyle() {
+    if (this.showPopup) {
+      return "block";
+    } else {
+      return "none";
+    }
+  }
+
+
+  areaInfo: AreaInfo[] = [];
+  save() {
+    this.areaInfo.push(
+      {
+        pageNumber: this.dataPageNumber,
+        rect: this.rect
+      }
+    );
+    this.showPopup = false;
+    console.log(this.areaInfo);
+  }
+
+  cancel(event) {
+    let rectId = this.element.getAttribute('id');
+    $('#'+rectId).remove();
+    this.showPopup = false;
+  }
+
 
 }
 
 interface Position {
   x: number;
   y: number;
+}
+
+
+interface Rectangle {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  width: number;
+  height: number;
+}
+
+
+interface AreaInfo {
+  pageNumber: number;
+  rect: Rectangle;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ClickOutsideDirectiveDirective } from './click-outside-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClickOutsideDirectiveDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     PdfViewerModule
   ],
   providers: [],
